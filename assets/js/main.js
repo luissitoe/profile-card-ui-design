607ad6dae8ace21d509560bab95e0857c63f1ae7
Luís Sitoe <70146258+luissitoe@users.noreply.github.com>
2023-06-05 18:04:25 +0200

    add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    themeToggle.classList.replace("bxs-moon", "bxs-sun");
+  } else {
+    themeToggle.classList.replace("bxs-sun", "bxs-moon");
+  }
+});
